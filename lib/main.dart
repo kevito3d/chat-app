2760ca810751a7d7914f60a2baff84260114526e
kevito3d <kevincove98@gmail.com>
2023-02-09 17:23:53 -0500

    add providers al main (global scope)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-import 'package:chat_real_time/services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:chat_real_time/routes/routes.dart';
-import 'package:provider/provider.dart';
+import 'package:chat_real_time/services/socket_service.dart';
+import 'package:chat_real_time/services/auth_service.dart';
+import 'package:chat_real_time/services/chat_service.dart';
 
 void main() => runApp(const MyApp());
 
@@ -14,6 +16,9 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AuthService()),
+        ChangeNotifierProvider(create: (_) => SocketService()),
+        ChangeNotifierProvider(create: (_) => ChatService()),
+
       ],
       child: MaterialApp(
         title: 'Chat App',
