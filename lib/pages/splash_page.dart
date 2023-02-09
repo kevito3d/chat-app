22c6ee7beb1bc3ed7ebc5d28b90bc92cac8953e8
kevito3d <kevincove98@gmail.com>
2023-02-09 17:26:22 -0500

    add conect to socker in splash if i logged

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:chat_real_time/pages/login_page.dart';
 import 'package:chat_real_time/pages/user_page.dart';
 import 'package:chat_real_time/services/auth_service.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:chat_real_time/services/socket_service.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -24,12 +25,16 @@ class SplashPage extends StatelessWidget {
 
   Future checkLoginState(BuildContext context) async {
     final authServise = Provider.of<AuthService>(context, listen: false);
+    final socketService= Provider.of<SocketService>(
+      context,listen: false
+    );
     final autenticado = await authServise.isLoggedIn();
     if (autenticado) {
+      socketService.connect();
       Navigator.pushReplacement(
           context,
           PageRouteBuilder(
-              pageBuilder: (_, __, ___) =>  UserPage(),
+              pageBuilder: (_, __, ___) =>  const UserPage(),
               transitionDuration: const Duration(milliseconds: 0)));
     } else {
       Navigator.pushReplacement(
