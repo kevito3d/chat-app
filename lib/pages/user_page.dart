4111d5e9c93b7ce5d24a00a45b367403056fc472
kevito3d <kevincove98@gmail.com>
2023-02-09 17:28:34 -0500

    add get all users and add writting stated

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -1,8 +1,12 @@
-import 'package:chat_real_time/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_real_time/models/user_model.dart';
 import 'package:provider/provider.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
+
+import 'package:chat_real_time/services/auth_service.dart';
+import 'package:chat_real_time/services/chat_service.dart';
+import 'package:chat_real_time/services/socket_service.dart';
+import 'package:chat_real_time/services/user_service.dart';
 
 class UserPage extends StatefulWidget {
   const UserPage({super.key});
@@ -14,14 +18,42 @@ class UserPage extends StatefulWidget {
 class _UserPageState extends State<UserPage> {
   final RefreshController _refreshController =
       RefreshController(initialRefresh: false);
-  final users = [
-    User(online: true, name: "Kevin", email: "kevin@gmail.com", uid: "1"),
-    User(online: false, name: "Juan", email: "juan@hotmail.com", uid: "2"),
-    User(online: true, name: "Pedro", email: "peditro@outlook.com", uid: "3"),
-  ];
+  List<User> users = [];
+
+  final userService = UserService();
+  late SocketService socketService;
+  late AuthService authService;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    socketService = Provider.of<SocketService>(context, listen: false);
+    authService = Provider.of<AuthService>(context, listen: false);
+
+    _cargarUsuarios();
+
+    socketService.socket.on("user-writting", _listenWritting);
+  }
+
+  void _listenWritting(dynamic payload) {
+    if (payload["to"] == authService.user!.uid) {
+      setState(() {
+        for (var user in users) {
+          if (user.uid == payload["from"]) {
+            user.writting = payload["writting"];
+          }
+        }
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final authServise = Provider.of<AuthService>(context);
+    final socketService = Provider.of<SocketService>(
+      context,
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text(authServise.user!.name),
@@ -31,6 +63,7 @@ class _UserPageState extends State<UserPage> {
         leading: IconButton(
           icon: const Icon(Icons.exit_to_app),
           onPressed: () {
+            socketService.disconnect();
             Navigator.pushReplacementNamed(context, '/login');
             AuthService.deleteToken();
           },
@@ -38,7 +71,9 @@ class _UserPageState extends State<UserPage> {
         actions: [
           Container(
             margin: const EdgeInsets.only(right: 10),
-            child: const Icon(Icons.check_circle, color: Colors.lightGreen),
+            child: socketService.serverStatus == ServerStatus.online
+                ? const Icon(Icons.check_circle, color: Colors.lightGreen)
+                : const Icon(Icons.offline_bolt, color: Colors.red),
             // child: Icon(Icons.offline_bolt, color: Colors.red),
           ),
         ],
@@ -65,7 +100,10 @@ class _UserPageState extends State<UserPage> {
 
   void _cargarUsuarios() async {
     // monitor network fetch
-    await Future.delayed(const Duration(milliseconds: 1000));
+    // await Future.delayed(const Duration(milliseconds: 1000));
+    users = await userService.getUsers();
+    setState(() {});
+
     // if failed,use refreshFailed()
     _refreshController.refreshCompleted();
   }
@@ -81,7 +119,28 @@ class _UserPageState extends State<UserPage> {
     return ListTile(
       title: Text(user.name,
           style: const TextStyle(color: Colors.white, fontSize: 20)),
-      subtitle: Text(user.email, style: const TextStyle(color: Colors.white54)),
+      subtitle: Row(
+        children: [
+          Expanded(
+              flex: 2,
+              child: Text(user.email,
+                  style: const TextStyle(color: Colors.white54))),
+          const SizedBox(width: 10),
+          Expanded(
+            flex: 1,
+            child: user.writting
+                ? Container(
+                  // color: Colors.green.shade100,
+                  child: const Text(
+                      "escribiendo",
+                      style: TextStyle(color: Colors.white54),
+                      textAlign: TextAlign.end ,
+                    ),
+                )
+                : const SizedBox.shrink(),
+          )
+        ],
+      ),
       leading: CircleAvatar(
         child: Text(
           user.name.substring(0, 2).toUpperCase(),
@@ -98,6 +157,14 @@ class _UserPageState extends State<UserPage> {
             color: user.online ? Colors.green : Colors.red,
             borderRadius: BorderRadius.circular(100)),
       ),
+      onTap: () {
+        final chatService = Provider.of<ChatService>(
+          context,
+          listen: false,
+        );
+        chatService.userTo = user;
+        Navigator.pushNamed(context, '/chat');
+      },
     );
   }
 }
